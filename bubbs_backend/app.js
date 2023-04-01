78bf4d4d9f798b2f25e429b47a016fb9ebdb844c
PettkoHarsanyi <pettko.harsanyi@gmail.com>
2023-04-01 05:29:32 +0200

    Backend now with express.js

---
+++ bubbs_backend/app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser');
+var cors = require('cors')
+const port = 3000
+
+app.use(cors())
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+
+app.get('/getplayers', (req, res) => {
+    res.send(readData());
+})
+
+app.post("/saveplayer", (req, res) => {
+    var newPlayer = req.body;
+
+    var data = readData();
+    var isAlready = false;
+    
+    data = data.map((player,index)=>{
+        if(player.name == newPlayer.name){
+            isAlready = true;
+            console.log(newPlayer.points + " " + player.points);
+            if(newPlayer.points > player.points){
+                console.log("nagyobb az uj")
+                return {name: player.name, points: newPlayer.points}
+            }
+        }
+        return player;
+    })
+
+    if(!isAlready){
+        data.push(newPlayer);
+    }
+
+    writeData(data);
+
+    res.send(data)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+function readData() {
+    const fs = require('fs');
+
+    let data = fs.readFileSync('players.json');
+    let json = JSON.parse(data);
+
+    return json;
+}
+
+function writeData(data) {
+    const fs = require('fs');
+    
+    let stringified_data = JSON.stringify(data);
+
+    fs.writeFileSync('players.json',stringified_data)
+}
--- bubbs_backend/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-$json = file_get_contents('players.json');
-
-echo $json;
-
-?>
